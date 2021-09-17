d4e589607ee0bc72e5f9c59256cbc8c5741b7878
vixin-pennut <85075873+vixin-pennut@users.noreply.github.com>
2021-09-17 12:11:08 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php $title = "Etusivu"; ?>
+<?php $body_class = "etusivu"; ?>
+
+<?php require("header.php"); ?>
+	<main>
+		<div class="first-page-grid main-grid">
+			<div class="grid-item box-item" id="etusivupalkki">
+				<h1>Vixin pentueet</h1>
+			</div>
+			<div class="grid-item" id="etusivukuva"></div>
+		</div>
+		<span class="image-copyright etusivu-copyright">Kuva &#169; <a href="https://www.ainovakkilainen.fi">Aino Vakkilainen</a></span>
+	</main>
+<?php require("footer.php"); ?>
